26a77edd11de9bcdb0559a4c7bad804d2607741f
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-10-12 00:41:25 -0300

    build: installing dependencies and building structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/blog" element={ <Blog /> } />
+    </Routes>
+  )
+}
+
+export default App;
