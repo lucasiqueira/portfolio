0d3adf5adce4ed1b8ac96e90faead0cd08054350
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-10-14 01:02:09 -0300

    feat: implements contact form

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Contact: React.FC = () => {
   return (
     <>
       <Header />
-      <div>Informações de contato</div>
-      <p>Passa um ZAP!</p>
+      <form action="">
+        <label htmlFor="name">
+          <p>Nome</p>
+          <input type="text" name="name" id="name" />
+        </label>
+        <label htmlFor="email">
+          <p>E-mail</p>
+          <input type="email" name="email" id="email" />
+        </label>
+        <label htmlFor="subject">
+          <p>Assunto</p>
+          <input type="text" name="subject" id="subject" />
+        </label>
+        <label htmlFor="message">
+          <p>Mensagem</p>
+          <textarea name="message" id="message" cols={30} rows={10}></textarea>
+        </label>
+        <input type="submit" value="Enviar"/>
+      </form>
+      <Footer />
     </>
   )
 }
