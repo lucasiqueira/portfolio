5de155fd8869672107011fbfc3556761eb8bb534
Lucas Siqueira <lucasiqueira@Lucass-MacBook-Air.local>
2023-11-23 04:11:35 -0300

    feat: implements contact page

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { SettingsContext } from '../contexts/SettingsContext';
+import contactInfo from '../data/contactInfo.json';
+import arrowDown from "../assets/images/icons/arrow-down.svg";
+
+import "../styles/pages/Contact.css";
 
 const Contact: React.FC = () => {
+  const settingsContext = React.useContext(SettingsContext);
+  const actualLanguage = settingsContext.language;
+
+  let contact;
+  if (actualLanguage === 'pt') {
+    contact = contactInfo.texts.pt;
+  } else {
+    contact = contactInfo.texts.en;
+  }
   return (
     <>
       <Header />
-      <main>
-        <form action="">
+      <main className="main-contact">
+        <h1 className="contact-title">{ contact.title }</h1>
+        <div className="contact-subtitle-container">
+          <p className="contact-subtitle">{ contact.subtitle }</p>
+          <img src={ arrowDown } alt="" className="contact-subtitle-image" />
+        </div>
+        {/* <form action="">
           <label htmlFor="name">
             <p>Nome</p>
             <input type="text" name="name" id="name" />
@@ -25,7 +44,7 @@ const Contact: React.FC = () => {
             <textarea name="message" id="message" cols={30} rows={10}></textarea>
           </label>
           <input type="submit" value="Enviar"/>
-        </form>
+        </form> */}
       </main>
       <Footer />
     </>
